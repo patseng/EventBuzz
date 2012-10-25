c40ba6db7ebfaccb8d94fa58afc0aa78ab652f5c
Peter Tseng <patseng@stanford.edu>
2012-10-25 14:37:24 -0700

    The skeleton for the map page is complete.  We have to read the jquery mobile docs so fix the visual glitch (we don't want the nag bar to fade in and out just the content)

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,4 +3,8 @@ class MainController < ApplicationController
     # this is an empty method, but will become clear why we have done this work
     # to set it up this way later
   end
+  
+  def map
+    # this is empty for now
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 EventBuzz::Application.routes.draw do
   root :to => 'main#index'
+  
+  match "/map" => "main#map", :as => :map
 end
