3a9fa6625e208d7c8d233efcb6f0d0e4b6925128
Peter Tseng <patseng@stanford.edu>
2012-10-24 17:47:03 -0700

    Set up Jquery Mobile!

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ group :assets do
 end
 
 gem 'jquery-rails'
+gem 'jquery_mobile_rails'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,6 @@
+class MainController < ApplicationController
+  def index
+    # this is an empty method, but will become clear why we have done this work
+    # to set it up this way later
+  end
+end
