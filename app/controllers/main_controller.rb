97c7b17b8ec18c751cf6ac5c7e6493971d786fb4
Peter Tseng <patseng@stanford.edu>
2012-10-26 14:08:06 -0700

    Refactored to a single page application.  No need for ajax for the navigation bar.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,15 +1,6 @@
 class MainController < ApplicationController
   def index
-    # this is an empty method, but will become clear why we have done this work
-    # to set it up this way later
+    @events = ["Obama Talk", "Romney Talk", "Big Bird Talk", 
+              "Basketball Game", "Big Game", "Tennis Match", "CS Career Fair"]
   end
-  
-  def map
-    # this is empty for now
-  end
-  
-  def find
-    @events = ["Obama Talk", "Romney Talk", "Big Bird Talk", "Basketball Game", "Big Game", "Tennis Match", "CS Career Fair"]
-  end
-    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 EventBuzz::Application.routes.draw do
   root :to => 'main#index'
-  
-  match "/map" => "main#map"
-  match "/find" => "main#find"
 end
