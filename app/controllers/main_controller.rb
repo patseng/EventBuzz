c1721a6a599d28d1ac9fc0d6d4fc3f518a106ddb
Peter Tseng <patseng@stanford.edu>
2012-10-28 13:18:20 -0700

    Dummy Data for My Events Page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,5 +2,8 @@ class MainController < ApplicationController
   def index
     @events = ["Obama Talk", "Romney Talk", "Big Bird Talk", 
               "Basketball Game", "Big Game", "Tennis Match", "CS Career Fair"]
+              
+    @my_events_today = ["Obama Talk", "Dodgeball Tournament"]
+    @my_events_tomorrow = ["Tennis Match"]
   end
 end
