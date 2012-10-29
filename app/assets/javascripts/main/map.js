f540e381b90b06311b3409e877c0fdea0e66704d
Peter Tseng <patseng@stanford.edu>
2012-10-28 21:23:58 -0700

    Add markers for map!

--- app/assets/javascripts/main/map.js
+++ app/assets/javascripts/main/map.js
@@ -1,12 +1,30 @@
 $(function() {
   var mapOptions = {
-    center: new google.maps.LatLng(-34.397, 150.644),
-    zoom: 8,
+    center: new google.maps.LatLng(37.42841, -122.16960),
+    zoom: 15,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
   var map = new google.maps.Map(document.getElementById("map_canvas"),
   mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(37.42137,	-122.1719),
+    map: map,
+    title: "Dodgeball Tournament!"
+  });
+  
+  var contentString = '<div id="content">' +
+                        '<h1>Dodgeball Tournament!</h1>' +
+                      '</div';
+                      
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+  
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map,marker);
+  });
 
   var the_height = ($(window).height() - 
                     $(this).find('[data-role="header"]').height() - 
