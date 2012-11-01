5e0ead7c5d228b26817f2d2595f22c1d928a5e9c
Sanjay Padval <sanjaypadval@yahoo.com>
2012-10-31 17:55:28 -0700

    My events page now communicates with DB

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,8 +2,8 @@ class MainController < ApplicationController
   def index
     @events = ["Obama Talk", "Romney Talk", "Big Bird Talk", 
               "Basketball Game", "Big Game", "Tennis Match", "CS Career Fair"]
-              
-    @my_events_today = ["Obama Talk", "Dodgeball Tournament"]
-    @my_events_tomorrow = ["Tennis Match"]
+     
+    @my_events_today = Event.all
+    @my_events_tomorrow = ["Poop Match"]
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+		Event.create([{:event_title =>"Obama Talk"}, {:event_title => "Dodgeball Tournament"}, 
+			{:event_title => "Tennis Match"}])
