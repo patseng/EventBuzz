237c2df2f71b7ef699b81087c541ee73bff5eab4
Peter Tseng <patseng@stanford.edu>
2012-11-02 02:55:42 -0700

    Removed clutter from map

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121102045418) do
+ActiveRecord::Schema.define(:version => 20121102050809) do
 
   create_table "events", :force => true do |t|
     t.string   "event_title"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(:version => 20121102045418) do
     t.datetime "updated_at",     :null => false
     t.float    "latitude"
     t.float    "longitude"
+  end
+
+  create_table "rsvps", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "event_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
