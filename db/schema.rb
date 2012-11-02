ce9ed980eddb5a964e47de3c9e8e5fb9ffa7f65e
Peter Tseng <patseng@stanford.edu>
2012-11-02 10:56:51 -0700

    Fixed schema for Rsvps.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121102050809) do
+ActiveRecord::Schema.define(:version => 20121102175520) do
 
   create_table "events", :force => true do |t|
     t.string   "event_title"
