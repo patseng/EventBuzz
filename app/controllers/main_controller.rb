02423a33c321f4faa14ef1e371356f8f96d5c29f
Sanjay Padval <sanjaypadval@yahoo.com>
2012-11-01 19:31:17 -0700

    Linked find events page to database and seeded database with new events

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,6 +4,7 @@ class MainController < ApplicationController
               "Basketball Game", "Big Game", "Tennis Match", "CS Career Fair"]
      
     @my_events_today = Event.all
+    @all_events = Event.all
     @my_events_tomorrow = ["Poop Match"]
     @active_users = User.all
   end
