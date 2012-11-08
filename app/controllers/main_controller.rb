5d354c43c005033970d564f0cd3382e1136fb4b2
Peter Tseng <patseng@stanford.edu>
2012-11-08 00:04:27 -0800

    User flow for when they aren't signed in and redesign of login page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,6 +6,7 @@ class MainController < ApplicationController
     @active_users = User.all
     @event_categories = {:Sports => @all_events[0...4], :Talks => @all_events[5...9]}
     gon.rabl "app/views/events/map.json.rabl", as: "events"
+    gon.signed_in = if current_user then true else false end
   end
 
   def get_event_detail
