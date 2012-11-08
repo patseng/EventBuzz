5943faca4afa681f6a8c4ea2e9648bf81ac68116
Peter Tseng <patseng@stanford.edu>
2012-11-07 23:42:44 -0800

    Fixed not going bug.

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -6,7 +6,8 @@ class RsvpsController < ApplicationController
   end
   
   def destroy 
-    rsvp = Rsvp.find_by_user_id_and_event_id(current_user.id, params[:event_id])
+    rsvp = Rsvp.find_by_user_id_and_event_id(current_user.id, params[:format])
     rsvp.destroy if rsvp
+    render :nothing => true
   end
 end
