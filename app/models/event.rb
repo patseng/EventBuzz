507f9b92996b8509829b81a3a331192fc7830a3b
Peter Tseng <patseng@stanford.edu>
2012-11-12 23:49:21 -0800

    Created Friendship model and table to improve performance of the site.  Note the friendship table is directional.  Thus creating a friendship requires two records. Added some dependencies between models. New users save off Facebook friends that are in our database.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ActiveRecord::Base
-	has_many :rsvps
+	has_many :rsvps, :dependent => :destroy
 	has_many :users, :through => :rsvps
-	has_many :event_categories
+	has_many :event_categories, :dependent => :destroy
 	has_many :categories, :through => :event_categories
 	
 	geocoded_by :location
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,21 @@
 class User < ActiveRecord::Base
-
-  has_many :rsvps
+  has_many :friendships, :dependent => :destroy
+  has_many :friends, :through => :friendships
+  has_many :rsvps, :dependent => :destroy
   has_many :events, :through => :rsvps
 
   # rescue error
   def facebook
-    @facebook ||= Koala::Facebook::API.new(self.oauth_token)
-  end
-
-  def friends
-    @friends ||= self.facebook.get_connection("me", "friends")
+    begin
+      @facebook ||= Koala::Facebook::API.new(self.oauth_token)
+    rescue Koala::Facebook::APIError
+      
+    end
   end
 
   def friends_going(event)
     self.friends.select do |friend|
-      user = User.find_by_uid(friend['id'])
-      user and event.users.include? user
+      event.users.include? friend
     end
   end
 
@@ -28,6 +28,18 @@ class User < ActiveRecord::Base
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.email = auth.info.email
       user.location = auth.info.location
+      
+      if user.new_record?
+        fb_friends = user.facebook.get_connection("me", "friends")
+        fb_friends.each do |fb_friend|
+          friend = User.find_by_uid(fb_friend['id'])
+          if friend
+            user.save!
+            user.friendships.create(:friend_id => friend.id)
+            Friendship.create(:user_id => friend.id, :friend_id => user.id)
+          end
+        end
+      end
       user.save!
     end
   end
