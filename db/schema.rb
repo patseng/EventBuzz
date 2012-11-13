6286493fe81acc42e7028761aed7f1e8c3b95598
Peter Tseng <patseng@stanford.edu>
2012-11-13 15:57:04 -0800

    Added Tutorial skeleton for new users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121113015131) do
+ActiveRecord::Schema.define(:version => 20121113081511) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -60,8 +60,9 @@ ActiveRecord::Schema.define(:version => 20121113015131) do
     t.string   "location"
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
+    t.boolean  "finished_tutorial"
   end
 
 end
