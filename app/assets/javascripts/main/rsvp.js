a5defda4931db2817b61148fdd6f2581534ef73e
Peter Tseng <patseng@stanford.edu>
2012-11-19 16:59:21 -0800

    Changing the rsvp button to be more user friendly :)

--- app/assets/javascripts/main/rsvp.js
+++ app/assets/javascripts/main/rsvp.js
@@ -1,4 +1,41 @@
 $(function() {
+  function contains(events, event) {
+    var contains = false;
+    var length = gon.my_events.length;
+    for (var i = 0; i < length; i++) {
+      if (gon.my_events[i].id == gon.event.id) {
+        contains = true;
+        break;
+      }
+    }
+    return contains;
+  }
+
+  function updateButtonColors() {  
+    var going = contains(gon.my_events, gon.event);
+    if (!going) {
+      // make going white
+      $('#going').buttonMarkup({ theme: "d" });
+      
+      // make not_goint blue
+      $("#not_going").removeClass('ui-btn-hover-d')
+      $('#not_going').buttonMarkup({ theme: "b" });
+    }
+    else {
+      // make going blue
+      $('#going').removeClass('ui-btn-hover-d')
+      $('#going').buttonMarkup({ theme: "b" });
+      // make not going white
+      $('#not_going').buttonMarkup({ theme: "d" });
+    }
+  }
+  
+  $(document).on("pageshow", function(test) {
+    if ($.mobile.activePage.attr("id") == "event_detail") {
+      updateButtonColors();
+    }
+  });
+  
   $(document).on('click', "#going", function() {
     if (gon.signed_in) {
       $.ajax({
@@ -22,9 +59,15 @@ $(function() {
       if (!found) {
         // add event
         gon.my_events.push(gon.event);
+        updateButtonColors();
         updateMyEvents();
       }
       
+
+      // Well have to override jquerymobile in order to get an aesthetically pleasing color change...
+      // setTimeout(function() {
+      //   $('#going').attr("data-theme", "b").removeClass("ui-btn-up-d").addClass("ui-btn-up-b");
+      // }, 700)
     }
   });
   
@@ -42,11 +85,14 @@ $(function() {
       for (var i = 0; i < length; i++) {
         if (gon.my_events[i].id == gon.event.id) {
           gon.my_events.remove(i);
+          updateButtonColors();
           updateMyEvents();
           break;
         }
       }
     }
+    
+
   });
 
 });
