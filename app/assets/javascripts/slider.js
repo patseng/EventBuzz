b591ff25124ec41efc0b550fc195e9125d26e107
Sanjay Padval <sanjaypadval@yahoo.com>
2012-11-25 14:30:30 -0800

    fixed a couple things

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -1,21 +1,25 @@
 $(document).ready(function() {
-
-	/* basic - default settings */
-	$('.iosSlider').iosSlider();
 	
-	/* some custom settings */
-	$('.iosSlider').iosSlider({
-		snapToChildren: true,
-		scrollbar: true,
-		scrollbarHide: false,
-		desktopClickDrag: true,
-		scrollbarLocation: 'bottom',
-		scrollbarHeight: '6px',
-		scrollbarBackground: 'url(_img/some-img.png) repeat 0 0',
-		scrollbarBorder: '1px solid #000',
-		scrollbarMargin: '0 30px 16px 30px',
-		scrollbarOpacity: '0.85',
-		// onSlideChange: changeSlideIdentifier
-	});
+	// $('div').live('pageshow',function(event,ui){
+
+	// 	if ($(this).attr("id") == "find"){
+	/* basic - default settings */ 
+			$('.iosSlider').iosSlider();
+						/* some custom settings */
+			$('.iosSlider').iosSlider({
+				snapToChildren: true,
+				scrollbar: true,
+				scrollbarHide: false,
+				desktopClickDrag: true,
+				scrollbarLocation: 'bottom',
+				scrollbarHeight: '6px',
+				scrollbarBackground: 'url(_img/some-img.png) repeat 0 0',
+				scrollbarBorder: '1px solid #000',
+				scrollbarMargin: '0 30px 16px 30px',
+				scrollbarOpacity: '0.85',
+				// onSlideChange: changeSlideIdentifier
+			});
+		 // }
+	 // });
 
 });
