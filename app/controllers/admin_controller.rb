570c060417fff5f4148bd4133545bcab13d464a4
Peter Tseng <patseng@stanford.edu>
2012-11-28 18:04:39 -0800

    Data Viewing

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,4 +2,11 @@ class AdminController < ApplicationController
   def users
     @active_users = User.all
   end
+  
+  def data
+    @times = []
+    TimingTest.all.each do |test|
+      @times << test.detail_timestamp - test.find_timestamp
+    end
+  end
 end
